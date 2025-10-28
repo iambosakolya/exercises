e51a2d13747abefcb5ea465dd9f7c875fc860393
iambosakolya <10252209@stud.op.edu.ua>
2025-10-28 17:53:50 +0200

    feat: first exercise started

---
+++ algorithms and data structures/linked-lists/constructor.js
@@ -0,0 +1,67 @@
+// Write the Node Class and the LinkedList constructor
+
+// CREATE NODE CLASS HERE //
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  // CREATE LL CONSTRUCTOR HERE //
+  constructor(value) {
+    const node = new Node(value);
+    this.head = node;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  printList() {
+    let temp = this.head;
+    while (temp !== null) {
+      console.log(temp.value);
+      temp = temp.next;
+    }
+  }
+
+  getHead() {
+    if (this.head === null) {
+      console.log("Head: null");
+    } else {
+      console.log("Head: " + this.head.value);
+    }
+  }
+
+  getTail() {
+    if (this.head === null) {
+      console.log("Tail: null");
+    } else {
+      console.log("Tail: " + this.tail.value);
+    }
+  }
+
+  getLength() {
+    console.log("Length: " + this.length);
+  }
+}
+
+let myLinkedList = new LinkedList(4);
+
+myLinkedList.getHead();
+myLinkedList.getTail();
+myLinkedList.getLength();
+console.log("\nLinked List:");
+myLinkedList.printList();
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Head: 4
+    Tail: 4
+    Length: 1
+    
+    Linked List:
+    4
+
+*/
